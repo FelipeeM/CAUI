44c617f62b5874f33f01677c56a1506a85a6317a
FelipeeM <felipemendes0411@gmail.com>
2022-11-19 02:27:33 -0300

    changed ports

--- CAUI.ino
+++ CAUI.ino
@@ -2,9 +2,9 @@ const int pinoSensor = A0; //PINO UTILIZADO PELO SENSOR
 const int ldr = A1;//Atribui A0 a variável ldr
 int valorLidoUmid; //VARIÁVEL QUE ARMAZENA O PERCENTUAL DE UMIDADE DO SOLO
 //Porta ligada ao pino IN1 do modulo
-int porta_rele1 = 13;
+int porta_rele1 = 9;
 int valorldr = 0;
-int ledAlertIrrig = 12;
+int ledAlertIrrig = 47;
 
 int analogSoloSeco = 900; //VALOR MEDIDO COM O SOLO SECO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
 int analogSoloMolhado = 300; //VALOR MEDIDO COM O SOLO MOLHADO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
@@ -17,7 +17,7 @@ void setup(){
  Serial.begin(9600); //INICIALIZA A SERIAL
  pinMode(ldr, INPUT);
  pinMode(pinoSensor, INPUT);
- pinMode(ledAlertIrrig, INPUT);
+ pinMode(ledAlertIrrig, OUTPUT);
  pinMode(porta_rele1, OUTPUT);
  Serial.println("Lendo a umidade do solo..."); //IMPRIME O TEXTO NO MONITOR SERIAL
  delay(2000); //INTERVALO DE 2 SEGUNDOS
@@ -41,7 +41,7 @@ void loop(){
  delay(1000);  //INTERVALO DE 1 SEGUNDO PARA CHECAR A UMIDADADE DO SOLO
    Serial.println("-----Irrigação desligada-----"); 
 
-  while(valorLidoUmid < 50 & valorldr < 450 ){
+  while(valorLidoUmid < 50 & valorldr < 150 ){
     Serial.println("-----Irrigando-----"); 
     digitalWrite(ledAlertIrrig, HIGH); //Liga rele 1
     digitalWrite(porta_rele1, LOW); //Liga rele 1
